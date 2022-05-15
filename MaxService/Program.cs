27b8f7e893a9ade3ccc827c57c3de69a13c4a2f8
Marlon Salles <marlonsalles@hotmail.com>
2022-05-15 15:26:12 -0300

    Restrições em Generics

---
+++ MaxService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using Services;
+namespace MaxService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+            int max = calculationService.Max(list);
+            Console.Write("Max:");
+            Console.WriteLine(max);
+            string a = Console.ReadLine();
+        }
+    }
+}
---
+++ MaxService/Services/CalculationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Services
+{
+    internal class CalculationService
+    {
+        public int Max(List<int> list)
+        {
+            if(list == null)
+            {
+                throw new ArgumentException("Lista nula, nao existe itens na mesma");
+            }
+
+            int max = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
